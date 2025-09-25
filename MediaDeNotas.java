daf720ed6f6d171b6d36fdf0b4677e85dd534562
Fernanda Rodrigues Nunes <pet0160449@aluno.firjansenaisesi.com.br>
2025-09-24 21:31:38 -0300

    Subindo correções

---
+++ MediaDeNotas.java
@@ -0,0 +1,9 @@
+/*Entrada de 5 notas em um laço For. Se a nota for maior que 10, use Continue para ignorar. Se a nota for negativa, use Break, 
+para sair do loop;Calculpe e imprima a media valida das notas */
+    
+public class MediaDeNotas {
+
+    System.out.println("Digite a primeira nota: ");
+double n1 = scanner.nextInt();
+    
+}
